b68cf7ff5bd195b5a989899088b73ef2cd1c5839
JohnFighting <johnfighting@vip.126.com>
2015-12-19 20:16:07 +0800

    Initial Commit

---
+++ JFBaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFBaseProject
+//
+//  Created by JohnFighting on 15/12/19.
+//  Copyright © 2015年 john. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
